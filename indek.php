5258b9eec24e799544068f84c49cc62edce872a1
dimasazizh <dimasaziz140@gmail.com>
2021-11-02 12:20:47 +0700

    Memodifikasi file indek.php

---
+++ indek.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Metodologi Desain Perangkat Lunak Praktik B</title>
+</head>
+<body>
+    <h1>Belajar GIT Remote</h1>
+    <p>Uty best</p>
+</body>
+</html>
